d2af41c26bb47f6073af93d9a82f7101fc5fea4e
Megantara Sandana <65941064+SandanaGH20@users.noreply.github.com>
2023-11-13 16:29:39 +0700

    Update protfolio.py

--- protfolio.py
+++ protfolio.py
@@ -1,1 +1,65 @@
-print("megantara")
+import streamlit as st
+# Tampilkan gambar
+st.image("daniel-j-schwarz-JaiXLtzGomQ-unsplash.jpg", caption="Foto saya")
+# Judul halaman
+st.title("Megantara Sandana - IT Professional")
+
+# Subjudul halaman
+st.subheader("Keamanan Komputer dan Pemrograman")
+
+# Tentang saya
+st.markdown("""
+Halo, saya Megantara Sandana. Seorang IT profesional berpengalaman di bidang keamanan komputer dan pemrograman. Saya memiliki passion yang besar terhadap dunia teknologi, khususnya keamanan komputer, pemrograman, dan sosial media branding.
+
+Saya menyelesaikan pendidikan Ahli Madya Manajemen Informatika saya di Universitas Pembangunan Nasional Veteran Jakarta tahun 2012. Saat ini, saya bekerja sebagai Staf IT di Badan Nasional Pengelolaan Perbatasan Republik Indonesia, sebuah lembaga pemerintah yang bertugas mengatur dan mengawasi perbatasan negara.
+
+Saya percaya bahwa keamanan komputer adalah hal yang vital untuk melindungi data dan sistem informasi dari ancaman. Saya juga suka belajar hal-hal baru dan menciptakan sesuatu yang inovatif dan bermanfaat.
+""")
+
+# Pengalaman kerja
+st.header("Pengalaman Kerja")
+
+# Item 1
+st.markdown("""
+* Staf IT, Badan Nasional Pengelolaan Perbatasan Republik Indonesia (2012 - sekarang)
+""")
+
+# Item 2
+st.markdown("""
+* IT Specialist, Kementerian Dalam Negeri Republik Indonesia (2010 - 2012)
+""")
+
+# Pendidikan
+st.header("Pendidikan")
+
+# Item 1
+st.markdown("""
+* Ahli Madya Manajemen Informatika, Universitas Pembangunan Nasional Veteran Jakarta (2008 - 2012)
+""")
+
+# Item 2
+st.markdown("""
+* Sekolah Menengah Atas Negeri 1 Jakarta (2005 - 2008)
+""")
+
+# Kontak
+st.header("Kontak")
+
+# Item 1
+st.markdown("""
+* Email: megantara.sandana@gmail.com
+""")
+
+# Item 2
+st.markdown("""
+* LinkedIn: [Megantara Sandana](https://www.linkedin.com/in/megantara-sandana/)
+""")
+
+# Tombol sosial media
+st.markdown("""
+<div style="text-align: center;">
+<a href="https://www.facebook.com/megantara.sandana"><img src="https://img.icons8.com/fluent/48/000000/facebook-new.png" alt="Facebook"></a>
+<a href="https://www.twitter.com/megantara_s"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="Twitter"></a>
+<a href="https://www.instagram.com/megantara_s"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="Instagram"></a>
+</div>
+""")
